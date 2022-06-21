2a425670fbc604933edcbc8441923612dc4fb075
Rafael Ramirez <susurrus.ater@gmail.com>
2022-06-21 13:53:01 -0400

    Ejercicio 02

---
+++ ejercicio-02.js
@@ -0,0 +1,12 @@
+const nombre = "Rafael Ramirez",
+edad = 41,
+desarrollador = true,
+fechaNacimiento = new Date(1981, 05, 12),
+libroFavorito = {
+    titulo: "El caso de Charles Dexter Ward",
+    autor: "H.P. Lovecraft",
+    fecha: new Date(1941),
+    url: "https://es.wikipedia.org/wiki/El_caso_de_Charles_Dexter_Ward"
+};
+
+console.log(`Mi nombre es ${nombre}, edad ${edad}`)
